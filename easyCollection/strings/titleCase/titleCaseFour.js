30f5adb59788278fa90b7e22e79d5d948878b1cc
Mike Haid <haidmichaelj@gmail.com>
2025-09-08 06:45:33 -0500

    titleCase practice

--- easyCollection/strings/titleCase/titleCaseFour.js
+++ easyCollection/strings/titleCase/titleCaseFour.js
@@ -8,12 +8,17 @@ const allUpper = 'HERE IS MY HANDLE HERE IS MY SPOUT'
 
 const titleCase = (str) => {
     // Create a variable to hold the string and covert it to lower case
-    
+    let toLower = str.toLowerCase().split(' ')
+    // console.log(toLower)
     // Loop through the variable in order to return the first letter as upper case and add it to the string using slice()
+    for (let i = 0; i < toLower.length; i++){
+        toLower[i] = toLower[i][0].toUpperCase() + toLower[i].slice(1)
+        // console.log(toLower[i] = toLower[i][0].toUpperCase() + toLower[i].slice(1))
     }
-    // return the joined string
+    // // return the joined string
+    return toLower.join(' ')
 }
 
 console.log(titleCase(teaPot))
-console.log(titleCase(shortStout))
-console.log(titleCase(allUpper))
+// console.log(titleCase(shortStout))
+// console.log(titleCase(allUpper))
